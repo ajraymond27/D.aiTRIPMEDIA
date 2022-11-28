903899836998d1112f4195facc7329373f7b6b0a
ajraymond27 <41302344+ajraymond27@users.noreply.github.com>
2022-11-27 18:44:32 -0700

    app creation

---
+++ ar-app/pages/index.js
@@ -0,0 +1,35 @@
+import Head from 'next/head'
+import { Canvas } from "@react-three/fiber";
+import css from "../styles/Home.module.css";
+import LightBulb from "../components/LightBulb";
+import OrbitControls from "../components/OrbitControls";
+import Draggable from "../components/Draggable";
+import Floor from "../components/Floor";
+import Box from "../components/Box";
+import {Suspense} from "react";
+
+
+export default function Home() {
+  return (
+    <div className={css.scene}>
+      <Canvas
+        shadows={true}
+        className={css.canvas}
+        camera={{
+          position: [-6, 7, 7],
+        }}
+      >
+          <ambientLight color={"white"} intensity={0.2} />
+          <LightBulb position={[0, 3, 0]} />
+          <Draggable>
+          <Suspense fallback={null}>
+              <Box rotateX={3} rotateY={0.2} />
+          </Suspense>
+          </Draggable>
+          <OrbitControls/>
+          <Floor position={[0, -1, 0]} />
+      </Canvas>
+    </div>
+  );
+}
+
