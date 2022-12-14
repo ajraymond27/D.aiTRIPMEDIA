0e42d5075417914f4374b288e2f4317bbd505cbb
ajraymond27 <41302344+ajraymond27@users.noreply.github.com>
2022-12-13 22:22:28 -0600

    rewrites

--- ar-app/next.config.js
+++ ar-app/next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
 }
+
+rewrites: async () => [
+  {
+    source: "/public/medicine_bow.html",
+    destination: "/pages/api/pages.js",
+
+    source: "/public/alien.html",
+    destination: "/pages/api/pages.js",
+
+    source: "/public/canyonlands.html",
+    destination: "/pages/api/pages.js",
+
+    source: "/public/crown.html",
+    destination: "/pages/api/pages.js",
+  },
+],
 
 module.exports = nextConfig
