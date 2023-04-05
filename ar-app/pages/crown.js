2843966ea8b4b577163ebcbac998ccfe5c88bec8
ajraymond27 <41302344+ajraymond27@users.noreply.github.com>
2023-04-04 20:08:08 -0600

    oops

--- ar-app/pages/crown.js
+++ ar-app/pages/crown.js
@@ -1,10 +1,10 @@
-import ARQuicklook from '../components/BigSteppers/Crown';
+import Crown from '../components/BigSteppers/Crown';
 
 const MyPage = () => {
   return (
     <div>
       <h1>My Page</h1>
-      <ARQuicklook />
+      <Crown />
     </div>
   );
 };
