13af6b08e49ec123c73ed3fcec4b329997a1b84c
ajraymond27 <41302344+ajraymond27@users.noreply.github.com>
2023-04-06 16:26:21 -0600

    crown updates

--- ar-app/components/BigSteppers/Crown.jsx
+++ ar-app/components/BigSteppers/Crown.jsx
@@ -3,13 +3,13 @@ import React from 'react';
 const Crown = () => {
   return (
     <div style={{ margin: '0px', overflow: 'hidden', textAlign: 'center' }}>
-      <div>
-        <a rel="ar" href="https://d13wrvtzk1buxr.cloudfront.net/usdz/FinalFinalCrown.usdz">
-          <img />
-          <p style={{ display: 'block', marginLeft: 'auto', marginRight: 'auto', marginTop: '25%', fontSize: '32pt', fontWeight: 'bolder' }}>I AM NOT YOUR SAVIOR</p>
+      <div style={{ display: 'block', marginLeft: 'auto', marginRight: 'auto', marginTop: '5%'}}>
+        <a rel="ar" href="https://d13wrvtzk1buxr.cloudfront.net/usdz/BigSteppers/crown.usdz">
+          <img style={{width: '20%'}} src='https://d13wrvtzk1buxr.cloudfront.net/usdz/BigSteppers/kendrick.png'/>
+          <p style={{fontSize: '32pt', fontWeight: 'bolder' }}>THAT'S WHAT I CALL LOVE</p>
         </a>
       </div>
-      <audio src='https://d13wrvtzk1buxr.cloudfront.net/usdz/crown.mp3' controls autoPlay />
+      <audio src='https://d13wrvtzk1buxr.cloudfront.net/usdz/BigSteppers/crown.mp3' controls autoPlay />
     </div>
   ); 
 };
