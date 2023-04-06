f63af849348d328f71c5a0c9d79f7bcfdd146008
ajraymond27 <41302344+ajraymond27@users.noreply.github.com>
2023-04-06 17:05:39 -0600

    crown styles and added image

--- ar-app/components/BigSteppers/Crown.jsx
+++ ar-app/components/BigSteppers/Crown.jsx
@@ -4,12 +4,12 @@ const Crown = () => {
   return (
     <div style={{ margin: '0px', overflow: 'hidden', textAlign: 'center' }}>
       <div>
-        <a rel="ar" href="https://d13wrvtzk1buxr.cloudfront.net/usdz/FinalFinalCrown.usdz">
-          <img />
-          <p style={{ display: 'block', marginLeft: 'auto', marginRight: 'auto', marginTop: '25%', fontSize: '32pt', fontWeight: 'bolder' }}>I AM NOT YOUR SAVIOR</p>
+        <a rel="ar" href="https://d13wrvtzk1buxr.cloudfront.net/usdz/BigSteppers/crown.usdz">
+          <img src="https://d13wrvtzk1buxr.cloudfront.net/usdz/BigSteppers/kendrick.png" alt="Image" style={{ width: '25%', height: 'auto' }} />
+          <p style={{ display: 'block', marginLeft: 'auto', marginRight: 'auto', fontSize: '32pt', fontWeight: 'bolder' }}>I AM NOT YOUR SAVIOR</p>
         </a>
       </div>
-      <audio src='https://d13wrvtzk1buxr.cloudfront.net/usdz/crown.mp3' controls autoPlay />
+      <audio src="https://d13wrvtzk1buxr.cloudfront.net/usdz/BigSteppers/crown.mp3" controls autoPlay />
     </div>
   ); 
 };
