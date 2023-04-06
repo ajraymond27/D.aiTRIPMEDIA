7663fbd85f11ca8f597b8e985e71d0405ec89442
ajraymond27 <41302344+ajraymond27@users.noreply.github.com>
2023-04-06 15:56:14 -0600

    update filepath

--- ar-app/components/BigSteppers/Crown.jsx
+++ ar-app/components/BigSteppers/Crown.jsx
@@ -4,7 +4,7 @@ const Crown = () => {
   return (
     <div style={{ margin: '0px', overflow: 'hidden', textAlign: 'center' }}>
       <div>
-        <a rel="ar" href="https://d13wrvtzk1buxr.cloudfront.net/usdz/FinalCrown.usdz">
+        <a rel="ar" href="https://d13wrvtzk1buxr.cloudfront.net/usdz/FinalFinalCrown.usdz">
           <img />
           <p style={{ display: 'block', marginLeft: 'auto', marginRight: 'auto', marginTop: '25%', fontSize: '32pt', fontWeight: 'bolder' }}>I AM NOT YOUR SAVIOR</p>
         </a>
