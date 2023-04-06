d87610c5e5cf1f72f0c2c67b255d137a7a03dff3
ajraymond27 <41302344+ajraymond27@users.noreply.github.com>
2023-04-06 17:09:52 -0600

    TEXT UPDATE

--- ar-app/components/BigSteppers/Crown.jsx
+++ ar-app/components/BigSteppers/Crown.jsx
@@ -5,8 +5,8 @@ const Crown = () => {
     <div style={{ margin: '0px', overflow: 'hidden', textAlign: 'center' }}>
       <div>
         <a rel="ar" href="https://d13wrvtzk1buxr.cloudfront.net/usdz/BigSteppers/crown.usdz">
-          <img src="https://d13wrvtzk1buxr.cloudfront.net/usdz/BigSteppers/kendrick.png" alt="Image" style={{ width: 'auto', height: '60vh' }} />
-          <p style={{ display: 'block', marginLeft: 'auto', marginRight: 'auto', fontSize: '32pt', fontWeight: 'bolder' }}>I AM NOT YOUR SAVIOR</p>
+          <img src="https://d13wrvtzk1buxr.cloudfront.net/usdz/BigSteppers/kendrick.png" alt="Image" style={{ width: 'auto', height: '50vh' }} />
+          <p style={{ display: 'block', marginLeft: 'auto', marginRight: 'auto', fontSize: '32pt', fontWeight: 'bolder' }}>THAT'S WHAT I CALL LOVE</p>
         </a>
       </div>
       <audio src="https://d13wrvtzk1buxr.cloudfront.net/usdz/BigSteppers/crown.mp3" controls autoPlay />
