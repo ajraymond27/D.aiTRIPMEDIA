3d8feadd580359535c727d9cdb60691a660db4f5
ajraymond27 <41302344+ajraymond27@users.noreply.github.com>
2023-04-06 16:14:02 -0600

    added mp3

--- ar-app/components/BigSteppers/Crown.jsx
+++ ar-app/components/BigSteppers/Crown.jsx
@@ -9,8 +9,9 @@ const Crown = () => {
           <p style={{ display: 'block', marginLeft: 'auto', marginRight: 'auto', marginTop: '25%', fontSize: '32pt', fontWeight: 'bolder' }}>I AM NOT YOUR SAVIOR</p>
         </a>
       </div>
+      <audio src='https://d13wrvtzk1buxr.cloudfront.net/usdz/crown.mp3' controls autoPlay />
     </div>
-  );
+  ); 
 };
 
 export default Crown;
