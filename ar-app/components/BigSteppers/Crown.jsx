435028894e7a1353e5270dc5e9cdd090b52689b6
ajraymond27 <41302344+ajraymond27@users.noreply.github.com>
2023-04-06 16:45:56 -0600

    another try

--- ar-app/components/BigSteppers/Crown.jsx
+++ ar-app/components/BigSteppers/Crown.jsx
@@ -5,11 +5,11 @@ const Crown = () => {
     <div style={{ margin: '0px', overflow: 'hidden', textAlign: 'center' }}>
       <div style={{ display: 'block', marginLeft: 'auto', marginRight: 'auto', marginTop: '5%'}}>
         <a rel="ar" href="https://d13wrvtzk1buxr.cloudfront.net/usdz/BigSteppers/crown.usdz">
-          <img style={{width: '20%'}} src='https://d13wrvtzk1buxr.cloudfront.net/usdz/BigSteppers/kendrick.png'/>
+          <img style={{width: '20%'}} src="https://d13wrvtzk1buxr.cloudfront.net/usdz/BigSteppers/kendrick.png"/>
           <p style={{fontSize: '32pt', fontWeight: 'bolder' }}>THAT'S WHAT I CALL LOVE.</p>
         </a>
       </div>
-      <audio src='https://d13wrvtzk1buxr.cloudfront.net/usdz/BigSteppers/crown.mp3' controls autoPlay />
+      <audio src="https://d13wrvtzk1buxr.cloudfront.net/usdz/BigSteppers/crown.mp3" controls autoPlay />
     </div>
   ); 
 };
