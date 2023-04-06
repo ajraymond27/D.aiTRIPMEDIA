589e9480d1c79fe0844bd40def2acfef5c1b2a34
ajraymond27 <41302344+ajraymond27@users.noreply.github.com>
2023-04-05 20:07:26 -0600

    crown updates

--- ar-app/components/BigSteppers/Crown.jsx
+++ ar-app/components/BigSteppers/Crown.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const Crown = () => (
   <div>
     <a rel="ar" href="https://d13wrvtzk1buxr.cloudfront.net/usdz/Crown.usdz">
-      <img src="/kendrick.png" />
+        <img/>
+        <p style="display: block; margin-left: auto; margin-right: auto; margin-top: 25%; font-size: 32pt; font-weight: bolder;">I AM NOT YOUR SAVIOR</p>
     </a>
   </div>
 );
