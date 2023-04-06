f71d2fe2ee93d7abf2c3d1cd88e6330555eb822a
ajraymond27 <41302344+ajraymond27@users.noreply.github.com>
2023-04-06 15:52:00 -0600

    crown page

--- ar-app/components/BigSteppers/Crown.jsx
+++ ar-app/components/BigSteppers/Crown.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
-const Crown = () => (
-  <div>
-    <a rel="ar" href="https://d13wrvtzk1buxr.cloudfront.net/usdz/Crown.usdz">
-        <img/>
-        <p style="display: block; margin-left: auto; margin-right: auto; margin-top: 25%; font-size: 32pt; font-weight: bolder;">I AM NOT YOUR SAVIOR</p>
-    </a>
-  </div>
-);
+const Crown = () => {
+  return (
+    <div style={{ margin: '0px', overflow: 'hidden', textAlign: 'center' }}>
+      <div>
+        <a rel="ar" href="https://d13wrvtzk1buxr.cloudfront.net/usdz/FinalCrown.usdz">
+          <img />
+          <p style={{ display: 'block', marginLeft: 'auto', marginRight: 'auto', marginTop: '25%', fontSize: '32pt', fontWeight: 'bolder' }}>I AM NOT YOUR SAVIOR</p>
+        </a>
+      </div>
+    </div>
+  );
+};
 
 export default Crown;
---
+++ ar-app/pages/crown.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Crown from '../components/BigSteppers/Crown';
+
+const CrownPage = () => {
+  return (
+    <div>
+      <Crown />
+    </div>
+  );
+};
+
+export default CrownPage;
