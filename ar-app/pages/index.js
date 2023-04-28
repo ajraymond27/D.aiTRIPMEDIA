8707c1f08996120c97b13247c5e354cd2e0f2760
ajraymond27 <41302344+ajraymond27@users.noreply.github.com>
2023-04-28 16:43:05 -0600

    countdown timer

--- ar-app/pages/index.js
+++ ar-app/pages/index.js
@@ -1,35 +1,60 @@
-import { Canvas } from "@react-three/fiber";
-import css from "../styles/Home.module.css";
-import LightBulb from "../components/LightBulb";
-import OrbitControls from "../components/OrbitControls";
-import Draggable from "../components/Draggable";
-import Floor from "../components/Floor";
-import Box from "../components/Box";
-import {Suspense} from "react";
-import { Plane } from "three";
-
+import React, { useState, useEffect } from 'react';
+import { Canvas } from '@react-three/fiber';
+import css from '../styles/Home.module.css';
+import LightBulb from '../components/LightBulb';
+import OrbitControls from '../components/OrbitControls';
+import Floor from '../components/Floor';
+import { Text } from '@react-three/drei';
 
 export default function Home() {
+  const [countdown, setCountdown] = useState({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+
+  useEffect(() => {
+    // Set the date and time of the event you're counting down to
+    const countdownDate = new Date('2023-06-01T00:00:00Z').getTime();
+
+    const interval = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = countdownDate - now;
+
+      // Calculate days, hours, minutes, and seconds left until countdownDate
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      // Update the countdown state with the new values
+      setCountdown({ days, hours, minutes, seconds });
+    }, 1000);
+
+    // Clean up the interval when the component unmounts
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className={css.scene}>
       <Canvas
         shadows={true}
         className={css.canvas}
         camera={{
-          position: [-6, 7, 7],
+          position: [0, 0, 10],
         }}
       >
-          <ambientLight color={"white"} intensity={0.2} />
-          <LightBulb position={[0, 3, 0]} />
-            <Suspense fallback={null}>
-                <Box rotateX={3} rotateY={0.2} />
-            </Suspense>
-          <OrbitControls/>
-
-          
-          <Floor position={[0, -1, 0]} />
+        <ambientLight color={'white'} intensity={0.2} />
+        <LightBulb position={[0, 3, 0]} />
+        <OrbitControls />
+        <Floor position={[0, -1, 0]} />
+        <Text
+          color={'white'}
+          position={[0, 1, 0]}
+          fontSize={0.5}
+          maxWidth={300}
+          lineHeight={1}
+          textAlign={'center'}
+        >
+          {`${countdown.days} Dai ${countdown.hours} HOURS ${countdown.minutes} MINS ${countdown.seconds} SECS`}
+        </Text>
       </Canvas>
     </div>
   );
 }
-
