9817c3901f327da41d0b03c19001802ec93f0d73
ajraymond27 <41302344+ajraymond27@users.noreply.github.com>
2023-04-28 16:44:55 -0600

    New Floor

--- ar-app/components/Floor.jsx
+++ ar-app/components/Floor.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function Floor(props) {
   return (
-    <mesh {...props} recieveShadow={true}>
-      <boxBufferGeometry args={[20,1,10]} />
-      <meshPhysicalMaterial color='white' />
+    <mesh {...props} recieveshadow="true">
+      <boxBufferGeometry args={[50,1,50]} />
+      <meshPhysicalMaterial color='#404040' />
     </mesh>
   );
 }
