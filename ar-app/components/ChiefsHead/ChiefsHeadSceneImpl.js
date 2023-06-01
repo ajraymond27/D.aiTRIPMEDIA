2b8e31392dfc69a69edafe6e2b9737b629beda1f
ajraymond27 <41302344+ajraymond27@users.noreply.github.com>
2023-05-31 19:46:02 -0600

    added text

--- ar-app/components/ChiefsHead/ChiefsHeadSceneImpl.js
+++ ar-app/components/ChiefsHead/ChiefsHeadSceneImpl.js
@@ -3,7 +3,8 @@ import {Entity} from 'aframe-react';
 import {useState} from 'react';
 
 const AframeSceneImpl = () => {
-  const [imageSrc, setImageSrc] = useState('https://d13wrvtzk1buxr.cloudfront.net/360_images/chief.jpg');
+  // const [imageSrc, setImageSrc] = useState('https://d13wrvtzk1buxr.cloudfront.net/360_images/chief.jpg');
+  const [imageSrc, setImageSrc] = useState('chief.jpg');
 
   const handleImageChange = (e) => {
     setImageSrc(e.target.value);
@@ -13,6 +14,7 @@ const AframeSceneImpl = () => {
     <Entity>
       <a-scene>
         <a-sky src={imageSrc} rotation="0 -100 0"></a-sky>
+        <a-text font="exo2bold" value="Rocky Mountain National Park" width="1.5" position="0.1 2.15 -2" rotation="0 0 0"></a-text>
       </a-scene>
       <input type="text" onChange={handleImageChange} value={imageSrc}/>
     </Entity>
