c0e87525d0d7ee0bf13ea5f9fefb79185e8ec7de
ajraymond27 <41302344+ajraymond27@users.noreply.github.com>
2023-06-13 19:48:51 -0600

    crossorigin?

--- ar-app/components/ChiefsHead/ChiefsHeadSceneImpl.jsx
+++ ar-app/components/ChiefsHead/ChiefsHeadSceneImpl.jsx
@@ -13,7 +13,7 @@ const ChiefsHeadSceneImpl = () => {
   return (
     <Entity>
       <a-scene>
-        <a-sky src={imageSrc} rotation="0 -100 0"></a-sky>
+        <a-sky src={imageSrc} rotation="0 -100 0" crossorigin="anonymous"></a-sky>
         <a-text font="exo2bold" value="Rocky Mountain National Park" width="1.5" position="0.1 2.15 -2" rotation="0 0 0"></a-text>
       </a-scene>
       <input type="text" onChange={handleImageChange} value={imageSrc}/>
