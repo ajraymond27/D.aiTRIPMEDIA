a8253901f22a5e579d5c20823966c7361ab710b1
ajraymond27 <41302344+ajraymond27@users.noreply.github.com>
2024-05-14 16:49:28 -0600

    nvm rolling back last commit

--- ar-app/components/WildFlowerCarhartt/WildFlowerCarharttSceneImpl.js
+++ ar-app/components/WildFlowerCarhartt/WildFlowerCarharttSceneImpl.js
@@ -13,14 +13,6 @@ const WildFlowerCarharttSceneImpl = () => {
     <Entity>
       <a-scene>
         <a-sky src={imageSrc} rotation="0 -100 0" crossorigin="anonymous"></a-sky>
-
-        {/* Add entity for the HOME text */}
-        <a-entity
-          position="-2 1 -3"  // Adjust position as needed
-          rotation="0 0 0"     // Adjust rotation as needed
-          text="value: HOME; align: center; wrapCount: 20; color: white; font: https://cdn.aframe.io/fonts/Exo2Bold.fnt; width: 4"
-          link="href: www.daitripmedia.com"       // Link to your homepage
-        ></a-entity>
       </a-scene>
       <input type="text" onChange={handleImageChange} value={imageSrc}/>
     </Entity>
