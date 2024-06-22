a27562158765d5047f0c8828467f6da5abbd0429
ajraymond27 <41302344+ajraymond27@users.noreply.github.com>
2024-06-22 16:38:51 -0600

    Revert "navbar update"
    This reverts commit b710461c8c66c0f648217c89fe5693d6eff85b38.

--- ar-app/components/NavBar/Navbar.jsx
+++ ar-app/components/NavBar/Navbar.jsx
@@ -6,10 +6,8 @@ import styles from '../../styles/Navbar.module.css';
 const Navbar = () => {
   return (
     <nav className={styles.navbar}>
-        <Link href="/">
-          <img src="/logo.png" alt="DAI TRIP Logo" className={styles.logoImage} />
-        </Link>
-        <div className={styles.navLinks}>
+      <div className={styles.logo}>DAI TRIP</div>
+      <div className={styles.navLinks}>
         <a href="https://www.daitripmedia.com/">HOME</a>
         <a href="https://www.daitripmedia.com/merch">MERCH</a>
         <a href="https://www.daitripmedia.store/browse/">PRINTS</a>
--- ar-app/pages/_app.js
+++ ar-app/pages/_app.js
@@ -1,7 +1,6 @@
 // pages/_app.jsx
 import React from 'react';
 import '../styles/globals.css';
-import '@fontsource/oswald'; // Import the Oswald font
 import Navbar from '../components/NavBar/Navbar';
 import MobileNavbar from '../components/NavBar/MobileNavbar';
 import { useMediaQuery } from 'react-responsive';
