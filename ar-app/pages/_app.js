95304db61f92deaf6e249e48f4234be069e20dae
ajraymond27 <41302344+ajraymond27@users.noreply.github.com>
2024-06-22 21:43:17 -0600

    add vercel speed package

--- ar-app/pages/_app.js
+++ ar-app/pages/_app.js
@@ -5,6 +5,7 @@ import '@fontsource/oswald'; // Import the Oswald font
 import Navbar from '../components/NavBar/Navbar';
 import MobileNavbar from '../components/NavBar/MobileNavbar';
 import { useMediaQuery } from 'react-responsive';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function MyApp({ Component, pageProps }) {
   const isMobile = useMediaQuery({ maxWidth: 768 });
@@ -12,6 +13,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       {isMobile ? <MobileNavbar /> : <Navbar />}
+      <SpeedInsights/>
       <Component {...pageProps} />
     </>
   );
