9ef6be3fc784ca4a923302e63ff0be7c367ac1e0
ajraymond27 <41302344+ajraymond27@users.noreply.github.com>
2024-06-22 21:46:46 -0600

    vercel analytics

--- ar-app/pages/_app.js
+++ ar-app/pages/_app.js
@@ -5,7 +5,8 @@ import '@fontsource/oswald'; // Import the Oswald font
 import Navbar from '../components/NavBar/Navbar';
 import MobileNavbar from '../components/NavBar/MobileNavbar';
 import { useMediaQuery } from 'react-responsive';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 function MyApp({ Component, pageProps }) {
   const isMobile = useMediaQuery({ maxWidth: 768 });
@@ -14,6 +15,7 @@ function MyApp({ Component, pageProps }) {
     <>
       {isMobile ? <MobileNavbar /> : <Navbar />}
       <SpeedInsights/>
+      <Analytics/>
       <Component {...pageProps} />
     </>
   );
