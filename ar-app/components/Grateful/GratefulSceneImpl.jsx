00b68990ca279298d4b7dae5fe05b5ce656cb1c4
ajraymond27 <41302344+ajraymond27@users.noreply.github.com>
2024-10-02 19:52:30 -0600

    rotate image

--- ar-app/components/Grateful/GratefulSceneImpl.jsx
+++ ar-app/components/Grateful/GratefulSceneImpl.jsx
@@ -13,7 +13,7 @@ const GratefulSceneImpl = () => {
     <Entity>
       <a-scene>
         <a-sky src={imageSrc} rotation="0 -100 0" crossorigin="anonymous"></a-sky>
-        <a-text font="Oswald" value="GRATEFUL DAI" width="1.5" position="0.1 2 -2" rotation="0 0 0"></a-text>
+        <a-text font="Oswald" value="GRATEFUL DAI" width="1.5" position="0.1 2 -2" rotation="0 -30 0"></a-text>
       </a-scene>
       <input type="text" onChange={handleImageChange} value={imageSrc}/>
     </Entity>
