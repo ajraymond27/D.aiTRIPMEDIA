a21c8772f5782e4f1c35432d0cef211cd5894256
ajraymond27 <41302344+ajraymond27@users.noreply.github.com>
2025-01-20 15:36:28 -0700

    videosphere template

---
+++ ar-app/pages/videosphere.js
@@ -0,0 +1,13 @@
+    // pages/video.js
+    import React from 'react';
+    import VideoSceneImplementation from '../components/BasicTemplates/Aframe/VideoSphereSceneImpl';
+
+    const VideoPage = () => {
+    return (
+        <div>
+            <VideoSceneImplementation />
+        </div>
+    );
+    };
+
+    export default VideoPage;
