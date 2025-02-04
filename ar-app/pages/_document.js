da47987b22634d90bd5d90a2debe2615fa5f995a
ajraymond27 <41302344+ajraymond27@users.noreply.github.com>
2025-02-04 16:40:01 -0700

    remove unneeded script

--- ar-app/pages/_document.js
+++ ar-app/pages/_document.js
@@ -4,8 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* Add the A-Frame script */}
-        <script async src="path/to/your/script.js"></script>
       </Head>
       <body>
         <Main />
