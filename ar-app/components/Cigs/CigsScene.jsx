c35fe8c7a03541215593cf8a77ccdd62e3a40c68
ajraymond27 <41302344+ajraymond27@users.noreply.github.com>
2025-03-04 17:31:18 -0700

    setup shell components for Malboro Cigs Jacket

---
+++ ar-app/components/Cigs/CigsScene.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import CigsSceneImpl from './CigsSceneImpl';
+
+const CigsScene = () => {
+  return <CigsSceneImpl />;
+};
+
+export default CigsScene;
---
+++ ar-app/pages/cigs.js
@@ -0,0 +1,12 @@
+import ThreeScene from '../components/BasicTemplates/Three/ThreeScene';
+
+function CigsPage() {
+  return (
+    <div>
+      <h1>Simple Three.js Scene</h1>
+      <ThreeScene />
+    </div>
+  );
+}
+
+export default CigsPage;
